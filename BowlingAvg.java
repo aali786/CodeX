1eb9a7e882696a6cf83da871e5ff5fe9e9b0d439
aali786 <abazar786@gmail.com>
2018-02-28 21:17:30 -0500

    Create BowlingAvg.java

---
+++ BowlingAvg.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BowlingAverage {
+	public static void main(String[] args) {
+		Scanner jarvis = new Scanner(System.in);
+		String empty = "";
+		do {
+			String name;
+			System.out.println("What is the bowler's name? ");
+			name = jarvis.nextLine();
+		}while(!name.empty());
+		
+	}
+
+}
